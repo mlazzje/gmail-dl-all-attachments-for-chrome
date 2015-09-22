e913a4e149eb9deabd092a44f80c7d24928e29f5
mlazzje <mlazzje@gmail.com>
2015-09-22 14:29:13 +0200

    Download attachments by opening a new tab IN BACKGROUND ;)

--- util.js
+++ util.js
@@ -6,9 +6,23 @@
 function processMultipleFilesDownload(urls, duration) {
   setTimeout(function() {
     for(var i = 0; i < urls.length; i++) {
-      window.open(urls[i]);
+      downloadAttachment(urls[i]);
     }
   }, duration);
+}
+
+/**
+ * Run atachment download
+ * @param  {string} url    Url to download the attachment
+ */
+function downloadAttachment(url) {
+  var a = document.createElement("a");
+  a.href = url;
+  var evt = document.createEvent("MouseEvents");
+  //the tenth parameter of initMouseEvent sets ctrl key
+  evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+                              true, false, false, false, 0, null);
+  a.dispatchEvent(evt);
 }
 
 /**
